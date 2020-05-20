29c58eec4ede435425958ccf3723634237d840cf
17076Cam <63985024+17076Cam@users.noreply.github.com>
2020-05-20 13:49:54 +1200

    V1.1
    2nd Version

---
+++ programmingProject_V1.1.py
@@ -0,0 +1,31 @@
+#programmingProject_V1.0
+#Cameron Galbraith
+
+"""
+Changelog
+v1.1
+Setup variables
+"""
+
+#imports
+import random
+
+
+#variables
+
+trash = ["Plastic Bag","Plastic Container","Plastic Bottle","Pile of Glad wrap","Metal Tin"] #Trash you can pick up
+
+gMap = [
+["X","O","O","O","O"],
+["O","O","O","O","O"],
+["O","O","O","O","O"],
+["O","O","O","O","O"],
+["O","O","O","O","O"]
+]
+
+playerPos = (0,3) #position of the player
+
+#functions
+
+   
+#main routine
